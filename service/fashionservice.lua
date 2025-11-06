c7ca4d71d3d6a32ecf241572f2f44bc169ae23a7
xys20071111 <xys20071111@hotmail.com>
2025-11-06 22:45:13 +0800

    添加强行写入时装信息的方法

--- service/fashionservice.lua
+++ service/fashionservice.lua
@@ -1,4 +1,5 @@
 local FashionService = class("servic.FashionService", Service.BaseService)
+local cjson = require("cjson")
 
 function FashionService:initialize()
   self:_InitHandlers()
@@ -6,6 +7,7 @@ end
 
 function FashionService:_InitHandlers()
   self:BindEvent("fashion.updateData", self._FashionUpdate, self)
+  self:BindEvent("fashion.custom.updateData", self._CustomFashionUpdate, self)
   self:BindEvent("fashion.fashionReplaceReward", self._FashionReplaceReward, self)
   self:BindEvent("fashion.Equip", self._EquipFashion, self)
 end
@@ -21,10 +23,21 @@ function FashionService:EquipFashion(fashionTid, fashionState, heroId)
 end
 
 function FashionService:_FashionUpdate(ret, state, err, errmsg)
+  log(ret)
   if err ~= 0 then
     logError("Fashion Update failed err:" .. err .. errmsg)
   else
     local fashionInfo = dataChangeManager:PbToLua(ret, fashion_pb.TFASHIONLIST)
+    Data.fashionData:SetData(fashionInfo)
+    self:SendLuaEvent(LuaEvent.UpdateFashionInfo)
+  end
+end
+
+function FashionService:_CustomFashionUpdate(ret, state, err, errmsg)
+  if err ~= 0 then
+    logError("Fashion Update failed err:" .. err .. errmsg)
+  else
+    local fashionInfo = cjson.decode(ret) -- dataChangeManager:PbToLua(ret, fashion_pb.TFASHIONLIST)
     Data.fashionData:SetData(fashionInfo)
     self:SendLuaEvent(LuaEvent.UpdateFashionInfo)
   end
